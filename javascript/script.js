e999779530d7f7c5c3862842e16732c84b186975
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2026-02-24 15:14:33 +0600

    implement job count calculation and update display in job application tracker

--- javascript/script.js
+++ javascript/script.js
@@ -16,3 +16,30 @@ const mainContainer = document.querySelector("main");
 const filterSection = document.getElementById("job-results");
 
 const noJobAvailable = document.getElementById("no-job-available");
+
+function calculateCount() {
+  const jobCards = allCardSection.querySelectorAll(".cardContainer");
+  total.innerText = jobCards.length;
+
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+
+  tabJobCount();
+}
+
+
+
+function tabJobCount() {
+  const countElement = document.querySelector(".flex.justify-between p.text-gray-500");
+
+  const totalJobs = allCardSection.querySelectorAll(".cardContainer").length;
+
+  if (allTabBtn.classList.contains("bg-[#3B82F6]")) {
+    countElement.innerText = totalJobs + " jobs";
+  } else if (interviewTabBtn.classList.contains("bg-[#3B82F6]")) {
+    countElement.innerText = interviewList.length + " of " + totalJobs + " jobs";
+  } else if (rejectedTabBtn.classList.contains("bg-[#3B82F6]")) {
+    countElement.innerText = rejectedList.length + " of " + totalJobs + " jobs";
+  }
+}
+
